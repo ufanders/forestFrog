e80f29e6b557dbe75f40a87a9825c91ec0a25369
Anders Nelson <anders.k.nelson@gmail.com>
2019-07-18 12:04:02 -0400

    -Code cleanup.

--- forestFrog/forestFrog.ino
+++ forestFrog/forestFrog.ino
@@ -1,9 +1,8 @@
 // ==== XTC DAC sound stuff
-#include "CROAK.h";
 #include "XT_DAC_Audio.h";
+#include "CROAK.h"; //Frog croak sound.
 XT_DAC_Audio_Class DacAudio(25,0); // Use GPIO 25, one of the 2 DAC pins and timer 0
 XT_Wav_Class CROAK(CROAKWav);
-uint32_t DemoCounter=0;
 
 // ==== FastLED stuff
 #include <FastLED.h>
@@ -15,9 +14,9 @@ uint32_t DemoCounter=0;
 #define DATA_PIN_3 13
 #define DATA_PIN_4 12
 CRGB leds[NUM_LEDS];
-CRGB leds2[NUM_LEDS];
-CRGB leds3[NUM_LEDS];
-CRGB leds4[NUM_LEDS];
+//CRGB leds2[NUM_LEDS];
+//CRGB leds3[NUM_LEDS];
+//CRGB leds4[NUM_LEDS];
 #define FRAMES_PER_SECOND  60
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
@@ -26,9 +25,9 @@ unsigned long time_now[2] = {0, 0};
 char toggle = 0;
 
 // ==== Direct PWM stuff
-const int bigLedPins[] = {2, 0, 4, 17, 5, 18}; //we don't use pin 5 because of high-output erratum upon POR.
+const int bigLedPins[] = {2, 0, 4, 17, 5, 18}; //Pin 5 contains a high-output erratum upon POR.
 const int motorPins[] = {16, 19};
-const int pwmFreq = 25000; //above the audible frequency band so we can't hear any EMI through the speaker.
+const int pwmFreq = 25000; //above the audible frequency band so we can't hear PWM refresh through the speaker.
 const int pwmRes = 8;
 CRGB ledsDirect[2];
 int currentLed;
@@ -41,10 +40,10 @@ bool button2Down = false;
 bool button3Down = false;
 bool button4Down = false;
 
-Debounce Button1(buttonPins[0], 100); // Button1 debounced, default 50ms delay.
-Debounce Button2(buttonPins[1], 100); // Button2 debounced, default 50ms delay.
-Debounce Button3(buttonPins[2], 100); // Button1 debounced, default 50ms delay.
-Debounce Button4(buttonPins[3], 100); // Button2 debounced, default 50ms delay.
+Debounce Button1(buttonPins[0], 50); // Button1 debounced, 50ms delay.
+Debounce Button2(buttonPins[1], 50); // Button2 debounced, 50ms delay.
+Debounce Button3(buttonPins[2], 50); // Button1 debounced, 50ms delay.
+Debounce Button4(buttonPins[3], 50); // Button2 debounced, 50ms delay.
 
 //color stuff
 byte coolingValue;
@@ -70,9 +69,9 @@ void setup()
 
   //==== FastLED stuff
   FastLED.addLeds<LED_TYPE, DATA_PIN_1, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, DATA_PIN_2, COLOR_ORDER>(leds2, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, DATA_PIN_3, COLOR_ORDER>(leds3, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, DATA_PIN_4, COLOR_ORDER>(leds4, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, DATA_PIN_2, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, DATA_PIN_3, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, DATA_PIN_4, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE, 21, RGB>(ledsDirect, sizeof(ledsDirect)/sizeof(CRGB)).setCorrection(TypicalLEDStrip);
   pinMode(21, INPUT); //we're not using this pin so we don't need it toggling.
 
@@ -115,43 +114,19 @@ void loop()
   //==== XTC DAC sound stuff
   DacAudio.FillBuffer();
 
-  //if(CROAK.Playing == false)
   if(!digitalRead(buttonPins[0]) && CROAK.Playing == false)
   {
     CROAK.Speed = random(50, 125) / 100.0;
     DacAudio.Play(&CROAK);
   }
 
-  //==== light control stuff
-  /*
-  if(!digitalRead(buttonPins[1]))
-  {
-    gHue = 0;
-  }
-
-  if(!digitalRead(buttonPins[2]))
-  {
-    gHue = 63;
-  }
-
-  if(!digitalRead(buttonPins[3]))
-  {
-    gHue = 127;
-  }
-  
-  
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-  fill_rainbow( leds2, NUM_LEDS, gHue, 7);
-  fill_rainbow( leds3, NUM_LEDS, gHue, 7);
-  fill_rainbow( leds4, NUM_LEDS, gHue, 7);
-  */
+  //==== Color control stuff
   fill_rainbow( ledsDirect, 2, gHue, 7);
- 
 
-  //TODO: read pin states.
+  //==== Button stuff
   if(!Button4.read() && button4Down == false)
   {
-    //LED mode button pushed.
+    //Color mode button pushed.
     button4Down = true;
     
     colorMode++;
@@ -160,21 +135,18 @@ void loop()
     switch(colorMode)
     {
       case 0: //LEDs off.
-      coolingValue = 55;
-      sparkingValue = 192;
       brightnessValue = 0;
       FastLED.setBrightness(brightnessValue);
-
       break;
 
-      case 1: //Fire.
+      case 1: //Fire fire.
       coolingValue = 55;
       sparkingValue = 100;
       brightnessValue = 96;
       FastLED.setBrightness(brightnessValue);
       break;
 
-      case 2: //MDMA fire.
+      case 2: //Rave fire.
       coolingValue = 55;
       sparkingValue = 160;
       brightnessValue = 192;
@@ -195,8 +167,9 @@ void loop()
     button4Down = false;
   }
 
-  if(!Button3.read() && button3Down == false) //CHANGE EYES
+  if(!Button3.read() && button3Down == false)
   {
+    //Eye rotation button pushed.
     motorToggle ^= 1;
     
     digitalWrite(motorPins[0], motorToggle);
@@ -208,11 +181,12 @@ void loop()
     button3Down = false;
   }
 
-  //TODO: draw video frame.
+  //Render video frame.
   random16_add_entropy( random8());
   
   if(colorMode == 3)
   {
+    //Rotate base hue.
     static uint8_t hue = 0;
     hue++;
     CRGB darkcolor  = CHSV(hue,255,192); // pure hue, three-quarters brightness
@@ -220,8 +194,9 @@ void loop()
     gPal3 = CRGBPalette16( CRGB::Black, darkcolor, lightcolor, CRGB::White);
   }
   
-  Fire2012WithPalette(); // run simulation frame, using palette colors
-  
+  Fire2012WithPalette();
+
+  //Display video frame.
   FastLED.show();
 
   //show direct PWM output
@@ -237,62 +212,62 @@ void loop()
 
 void Fire2012WithPalette()
 {
-// Array of temperature readings at each simulation cell
+  // Array of temperature readings at each simulation cell
   static byte heat[NUM_LEDS];
 
   // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((coolingValue * 10) / NUM_LEDS) + 2));
-    }
+  for( int i = 0; i < NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((coolingValue * 10) / NUM_LEDS) + 2));
+  }
+
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  }
   
-    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  if( random8() < sparkingValue ) {
+    int y = random8(7);
+    heat[y] = qadd8( heat[y], random8(160,255) );
+  }
+
+  // Step 4.  Map from heat cells to LED colors
+  for( int j = 0; j < NUM_LEDS; j++) {
+    // Scale the heat value from 0-255 down to 0-240
+    // for best results with color palettes.
+    byte colorindex = scale8( heat[j], 240);
+
+    CRGB color;
+
+    switch(colorMode)
+    {
+      case 0: //all black.
+        color = ColorFromPalette( gPal0, 0);
+      break;
+
+      case 1: //fire.
+        color = ColorFromPalette( gPal1, colorindex);
+      break;
+
+      case 2: //MDMA fire.
+        color = ColorFromPalette( gPal2, colorindex);
+      break;
+
+      case 3: //FREAK OUT
+        color = ColorFromPalette( gPal3, colorindex);
+      break;
     }
     
-    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-    if( random8() < sparkingValue ) {
-      int y = random8(7);
-      heat[y] = qadd8( heat[y], random8(160,255) );
+    int pixelnumber;
+    if( gReverseDirection ) {
+      pixelnumber = (NUM_LEDS-1) - j;
+    } else {
+      pixelnumber = j;
     }
-
-    // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
-      // Scale the heat value from 0-255 down to 0-240
-      // for best results with color palettes.
-      byte colorindex = scale8( heat[j], 240);
-
-      CRGB color;
-
-      switch(colorMode)
-      {
-        case 0: //all black.
-          color = ColorFromPalette( gPal0, 0);
-        break;
-
-        case 1: //fire.
-          color = ColorFromPalette( gPal1, colorindex);
-        break;
-
-        case 2: //MDMA fire.
-          color = ColorFromPalette( gPal2, colorindex);
-        break;
-
-        case 3: //FREAK OUT
-          color = ColorFromPalette( gPal3, colorindex);
-        break;
-      }
-      
-      int pixelnumber;
-      if( gReverseDirection ) {
-        pixelnumber = (NUM_LEDS-1) - j;
-      } else {
-        pixelnumber = j;
-      }
-      
-      leds[pixelnumber] = color;
-      leds2[pixelnumber] = color; //we hack in a copy of the first strip data into the second strip.
-      leds3[pixelnumber] = color;
-      leds4[pixelnumber] = color;
-    }
+    
+    leds[pixelnumber] = color;
+    //leds2[pixelnumber] = color;
+    //leds3[pixelnumber] = color;
+    //leds4[pixelnumber] = color;
+  }
 }
